082ab58af874b63701b60ada2943630ebe5552a7
Adam Morgan <adam.morgan@efi.com>
2015-07-20 12:46:08 +0100

    Added support for custom aggregators.

--- aggr.py
+++ aggr.py
@@ -6,6 +6,7 @@ import sys
 import csv
 import datetime
 from collections import defaultdict
+from functools import partial, reduce
 from itertools import groupby
 
 parser = argparse.ArgumentParser('aggr')
@@ -16,12 +17,13 @@ parser.add_argument('-o', '--outfile')
 parser.add_argument('-d', '--field-delimiter')
 parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('--datetime-format')
+parser.add_argument('--initial-argument-for-custom-aggregator')
 parser.add_argument('--time-format')
 parser.add_argument('--date-format')
 
 parser.set_defaults(field_delimiter=',', verbose=False, 
         datetime_format='%Y-%m-%dT%H:%M:%s.%f%z', date_format='%Y-%m-%d', 
-        time_format='%H:%M:%s')
+        time_format='%H:%M:%s', initial_argument_for_custom_aggregator='0')
 
 args = parser.parse_args()
 
@@ -40,8 +42,6 @@ FUNCTION_MAP = {
 
 def parse_pattern():
     pattern = args.pattern.split(args.field_delimiter)
-    for p in pattern:
-        assert p in PATTERN_OPTIONS
     return pattern
 
 
@@ -54,19 +54,12 @@ def get_keyfunc():
 
 
 def infer_type(field):
-    try:
-        return int(field)
-    except ValueError as ex:
-        pass
-    try:
-        return float(field)
-    except ValueError as ex:
-        pass
-    try:
-        return datetime.datetime.strptime(field, args.datetime_format)
-    except ValueError as ex:
-        pass
-    
+    types = (int, float, partial(lambda f: datetime.datetime.strptime(f, args.datetime_format)))
+    for t in types:
+        try:
+            return t(field)
+        except ValueError as ve:
+            pass
     return field
 
 
@@ -82,13 +75,30 @@ def get_records():
         return sys.stdin
 
 
+def parse_aggregate_function(n):
+    try:
+        return FUNCTION_MAP[parse_pattern()[n]]
+    except KeyError as ke:
+        pass
+
+    r = eval(parse_pattern()[n])
+# r is of the form
+# def r(accumulator, value_from_iterator):
+#     return value_from_iterator
+    def logged_reduce(iterable):
+        logging.debug(r)
+        logging.debug(iterable)
+        return reduce(r, iterable, eval(args.initial_argument_for_custom_aggregator))
+    return logged_reduce    
+
+
 def aggregate_group(records):
     records = list(records)
     for n, _ in enumerate(records[0]):
         if parse_pattern()[n] == 'key':
             continue
         else:
-            func = FUNCTION_MAP[parse_pattern()[n]]
+            func = parse_aggregate_function(n)
             column = [record[n] for record in records]
             yield func(column)
 
